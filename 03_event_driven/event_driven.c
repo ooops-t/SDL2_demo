acb139bbd9e54999e6326d3ab3d0318779df78d9
taocheng <taocheng@hcfa.cn>
2022-08-11 10:18:06 +0800

    feat: 03_event_driven
    Signed-off-by: taocheng <taocheng@hcfa.cn>

---
+++ 03_event_driven/event_driven.c
@@ -0,0 +1,75 @@
+#include <SDL2/SDL.h>
+#include <stdio.h>
+
+// Define Window size
+#define WINDOW_WIDTH  (640)
+#define WINDOW_HEIGHT (480)
+
+int main(int argc, char *argv[])
+{
+  SDL_Window *window = NULL;
+  SDL_Surface *surface = NULL;
+  SDL_Surface *image = NULL;
+  // Main loop flag
+  uint8_t quit = 0; // false
+  // Event handle
+  SDL_Event e;
+
+  // Initialize SDL
+  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+    fprintf(stderr, "SDL initialize failed: %s\n", SDL_GetError());
+    return -1;
+  }
+
+  // Create window
+  window = SDL_CreateWindow("Load Image",
+			    SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+			    WINDOW_WIDTH, WINDOW_HEIGHT,
+			    SDL_WINDOW_SHOWN);
+  if (NULL == window) {
+    fprintf(stderr, "SDL create window failed: %s\n", SDL_GetError());
+    goto error_create_window;
+  }
+
+  // Get window surface
+  surface = SDL_GetWindowSurface(window);
+
+  // Fill the surface white
+  SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 0xFF, 0xFF, 0xFF));
+
+  // Load image
+  image = SDL_LoadBMP("./x.bmp");
+  if (NULL == image) {
+    fprintf(stderr, "SDL load bmp image failed: %s\n", SDL_GetError());
+    goto error_load_bmp;
+  }
+
+  while (!quit) {
+    while (SDL_PollEvent(&e) != 0) {
+      switch (e.type) {
+      case SDL_QUIT:
+	quit = 1;
+	break;
+      default:
+	break;
+      }
+    }
+
+    // Apply the image
+    SDL_BlitSurface(image, NULL, surface, NULL);
+
+    // Update the surface
+    SDL_UpdateWindowSurface(window);
+  }
+
+  SDL_FreeSurface(image);
+  image = NULL;
+ error_load_bmp:
+  // Destroy window
+  SDL_DestroyWindow(window);
+ error_create_window:
+  // Quit SDL subsystems
+  SDL_Quit();
+
+  return 0;
+}
